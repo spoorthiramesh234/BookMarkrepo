03b42e49ddebd84c0241b8ee334772fc6979eb3c
Namrathahvijay <namrathahvijay9740@gmail.com>
2026-02-24 13:09:18 +0530

    Connected frontend to Railway backend

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api/bookmarks"
+  baseURL: "https://backendforbook-production.up.railway.app/api/bookmarks"
 });
 
 export default API;
